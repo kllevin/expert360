00346782cc2f98952cd7d98bd13826f6fff50c09
Kate Levin <kllevin@gmail.com>
2015-07-21 17:43:56 +1000

    project boilerplate - grunt, scally, html

---
+++ Gruntfile.js
@@ -0,0 +1,47 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    watch: {
+
+      sass: {
+        files: './style/**/*.scss',
+        tasks: ['style']
+      },
+
+      css: {
+        files: ['./style/*.css']
+      }
+
+    },
+
+    sass: {
+      dist: {
+        options: {
+          style: 'expanded'
+        },
+        files: {
+          'style/style.css': 'style/style.scss'
+        }
+      }
+    },
+
+    autoprefixer: {
+      dist: {
+        files: {
+          'style/style.css': 'style/style.css'
+        }
+      },
+      options: ['last 2 versions', 'ie 9']
+    }
+
+  });
+
+  grunt.loadNpmTasks('grunt-autoprefixer');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+
+  grunt.registerTask('style', ['sass', 'autoprefixer']);
+  grunt.registerTask('default', ['style']);
+};
--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# expert360
+# Expert360
 Technical Test for Expert360
+
+To run this project you will need:
+- [Node](http://nodejs.org/)
+- [Node-Sass](https://npmjs.org/package/node-sass)
+- [Grunt-cli](http://gruntjs.com/)
+
+### Grunt Tasks
+
+- `grunt style` compile SASS and autoprefix 
+
+### TODO
+- GH Pages?
+- grunt to minify css, svg, html, js
+- grunt for UNCSS
+- grunt to optimise images
+- linting
